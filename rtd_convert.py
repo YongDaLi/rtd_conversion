7c6200aa092d0b3ad349effa724eeb33eed4d091
YongDaLi <yongdali314@gmail.com>
2019-05-30 14:31:48 +0800

    revised comments

--- rtd_convert.py
+++ rtd_convert.py
@@ -5,7 +5,7 @@ Professor Ho Ghim Wei
 
 Using RTD to measure cold junction temperature of data loggger on rooftop experiment.
 Implemented simple voltage divider circuit with 2V5 input (from PicoLog breakout)
-This program converts .csv with measured ADC voltages from RTD to temperature.
+This program converts .csv files with measured  voltages from the RTD to temperature.
 '''
 
 import math
